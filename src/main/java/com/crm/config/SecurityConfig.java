af3c8a2e142f23a6ca10538145395b35efb51577
Tony <gm10285@gmail.com>
2020-09-19 00:25:20 +0800

    add spring security config

---
+++ src/main/java/com/crm/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.crm.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}123").roles("ADMIN")
+                .and()
+                .withUser("manager").password("{noop}123").roles("manager")
+                .and()
+                .withUser("operator").password("{noop}123").roles("operator");
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.DELETE,"company").hasAnyRole("MANAGER", "SUPERUSER")
+                .antMatchers(HttpMethod.PUT,"company").hasAnyRole("MANAGER", "SUPERUSER")
+                .antMatchers(HttpMethod.POST,"company").hasAnyRole("OPERATOR", "SUPERUSER")
+                .antMatchers(HttpMethod.POST,"companys").hasAnyRole("OPERATOR", "SUPERUSER")
+                .antMatchers(HttpMethod.GET,"companys").hasAnyRole("OPERATOR", "MANAGER", "SUPERUSER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .disable()
+                .httpBasic()
+                .and()
+                .csrf()
+                .disable();
+    }
+
+}
+
